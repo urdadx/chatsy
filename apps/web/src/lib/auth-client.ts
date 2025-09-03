e050fae2be285dff92017768732a0b687e8643db
Nginze <kuugjonathan45@gmail.com>
2025-09-03 12:03:01 +0000

    update base url on auth client

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3001",
 
   plugins: [organizationClient(), polarClient()],
 });
